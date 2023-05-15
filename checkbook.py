82eadf427f51d65c26a130074a30f0a22b027747
Alfred W. S. Pirovits Jr <alfredpirovits@gmail.com>
2023-05-15 09:41:38 -0500

    Continue work on Project

--- checkbook.py
+++ checkbook.py
@@ -30,7 +30,7 @@ def continue_or_error(): #Loops through the options until user chooses to exit
             
         elif user_option == '3': # insert credit function
             credit()
-            break
+            
         elif user_option == '4': # insert exit function
             print('\nThanks, have a great day!')
             break 
@@ -41,27 +41,75 @@ def current_balance(): #read the current balance from the balance.txt file?
     with open('balance.txt', 'r') as f:
         last_line = f.readlines()[-1]
     print(f'\nYour current balance is {last_line}.\n')
+    continue_prompt()
 
 def debit():
     with open('balance.txt', 'r') as f:
-        last_line = int(f.readlines()[-1])
-        withdrawal = int(input(f'\nHow much would you like to wittdraw? '))
-    if withdrawal <= last_line:
-        balance = last_line - withdrawal
-        with open('balance.txt', 'a') as f:
-            f.writelines([f'\n{balance}'])  # if entry is less than balance then balance minus entry
-        print(f'\nYou have ${balance} remaining.\n')
+        last_line = float(f.readlines()[-1])
+        withdrawal = (input(f'\nHow much would you like to withdraw? '))
+    if isfloat(withdrawal) == True:
+        withdrawal = round(float(withdrawal), 2)
+        if withdrawal < 0:
+            print('You cannot withdrawal a negative number. That would be a DEPOSIT. ')
+            continue_prompt()
+        elif withdrawal <= last_line:
+            balance = last_line - withdrawal
+            with open('balance.txt', 'a') as f:
+                f.writelines([f'\n{balance}'])  # if entry is less than balance then balance minus entry
+            print(f'\nYou have Withdrawn ${withdrawal: .2f}:')
+            print(f'\nYou have ${balance: .2f} remaining.\n')
+            continue_prompt()
+        else:
+            print(f'\nYou only have ${last_line: .2f} in your account.\nWithdrawing ${withdrawal: .2f} will overdraft your account!\n\n')
+            continue_prompt()
     else:
-        print(f'\nYou only have ${last_line} in your account.\nWithdrawing ${withdrawal} will overdraft your account!\n')
+        print('Please type in a valid number: ')
+        continue_prompt()
 
      # else print your available balance is {} you will overdraw account
 
 def credit(): # adds the user input to the total balance
-    input(f'\nHow much would you like to deposit?')
+    with open('balance.txt', 'r') as f:
+        last_line = float(f.readlines()[-1])
+        deposit = input(f'\nHow much would you like to deposit? ')
+    if isfloat(deposit) == True:
+        deposit = round(float(deposit), 2)
+        if deposit >= 0:
+            balance = last_line + deposit
+            with open('balance.txt', 'a') as f:
+                f.writelines([f'\n{balance}'])
+            print(f'\nYou have deposited ${deposit: .2f}:')
+            print(f'\nYou have ${balance: .2f} remaining.\n')
+            continue_prompt()
+        elif deposit < 0:
+            print('You cannot deposit a negative number.  That would be a WITHDRAWAL! ')
+            continue_prompt()
+    else: 
+        print('Please type in a valid number: ')
+        continue_prompt()
 
 def exit():  #perhaps not needed but records and saves current balance
     print('\nThanks, have a great day!')
 
+def continue_prompt(): #prompts user to continue 
+    c = 0
+    while c != 'y':
+            c = input('Would you like to continue? (y/n) ')
+            if c == 'n':
+                global user_option
+                user_option = 4
+                break
+            elif c == 'y':
+                continue
+            else:
+                print('Please enter y or n')
+
+def isfloat(num):  #Checks user puts in valid float
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
 
 main()
 
